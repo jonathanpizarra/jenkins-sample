e1197efb628b219ecbaddcb2bdce36bd2715fa9d
Jonathan Pizarra <atanpizarra@gmail.com>
2022-10-04 09:57:03 +0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,6 +2,6 @@
 public class Sample {
 
     public static void main(String[] args) {
-        System.out.println("============= HELLLO WORLDDD!!!! ===dfdfdfdfdf==========");
+        System.out.println("=====wewewew======== HELLLO WORLDDD!!!! ===dfdfdfdfdf==========");
     }
 }
